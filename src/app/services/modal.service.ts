0e06798c9f162997240a6897fec8a1909e737207
Johan Garzon <johan14actylory@gmail.com>
2022-06-11 20:24:31 -0500

    open a modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -4,7 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalService {
+  private visible = false
 
-  visible = false
   constructor() { }
+
+  isModalOpen () {
+    return this.visible
+  }
+
+  toggleModal () {
+    this.visible = !this.visible
+  }
 }
