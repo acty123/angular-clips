89bbb881dab7afb43cb27c7294a9f6fcaf14d14d
Johan Garzon <johan14actylory@gmail.com>
2022-06-20 13:02:28 -0500

    add ElementRef to avoid css issues with another component

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -8,15 +8,15 @@ import { ModalService } from 'src/app/services/modal.service';
   // providers: [ModalService],
 })
 export class ModalComponent implements OnInit {
-  @Input() modalID =''
+  @Input() modalID = '';
 
-  constructor(public modal: ModalService) {
-    
+  constructor(public modal: ModalService, public el: ElementRef) {}
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
-  ngOnInit(): void {}
-
   closeModal() {
-    this.modal.toggleModal(this.modalID)
+    this.modal.toggleModal(this.modalID);
   }
 }
